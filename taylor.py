7ddd2aed006bd7859fe1c21affbe8d52baec267c
Samuel Admaw <samueladmaw23@gmail.com>
2025-11-24 16:10:22 +0300

    Added plots

---
+++ taylor.py
@@ -0,0 +1,106 @@
+from matplotlib import pyplot as plt
+from math import factorial
+
+
+import numpy as np
+import re
+
+FUNCTIONS = r"(sin|cos|tan|sec|csc|cot|log|ln|exp)"
+expr=input("Enter a function eg: sin(x), x**2, e**(x+1), (2x+1)**1/2 ...  \n f(x): ")
+def convert_to_numpy(expr):
+    expr = expr.replace("sin", "np.sin")
+    expr = expr.replace("cos", "np.cos")
+    expr = expr.replace("tan", "np.tan")
+    expr = expr.replace("ln", "np.log")   
+    expr = expr.replace("exp",   "np.exp")
+    expr = expr.replace("^", "**") 
+    return expr        
+
+def fix_functions(expr):
+    expr=expr.lower()
+    expr=expr.replace(" ","")
+
+
+    #change(3x=>3*x)
+
+    expr=re.sub(r"(\d)(x)",r"\1*\2",expr)
+
+    #change(x3=>x**3)
+
+    expr=re.sub(r"(x)(\d)",r"\1**\2",expr)
+
+    #change (ex=>e**x)
+
+    expr=re.sub(r"(e)(\d)",r"\1**\2",expr)
+
+    #change FUNCTIONS to proper form(sinx=>sin(x))
+
+    expr=re.sub(rf"{FUNCTIONS}x",r"\1(x)",expr)
+    expr=convert_to_numpy(expr)
+    return expr
+
+    
+def make_func(expr):
+    expr=fix_functions(expr)
+    def f(x):
+        return eval(expr,{"np":np,"x":x})
+    return f
+
+func=make_func(expr)
+
+
+def derivitive(f,a,h=1e-5,order=1):
+    if order==0:
+        return f(a)
+    elif order==1:
+        return (f(a+h)-f(a-h))/(2*h)
+    else:
+        def nth_derivative(f,a,n):
+            if n==1:
+                return (f(a+h)-f(a-h))/(2*h)
+            else:
+                return (nth_derivative(f,a+h,n-1)-nth_derivative(f,a-h,n-1))/(2*h)
+        return nth_derivative(f,a,order)
+         
+    
+
+def taylor_polynominal(func,a,n):
+   
+   def P(x):
+       result=0
+       for k in range(n+1):
+           term=derivitive(func,a,order=k)/factorial(k)*(x-a)**k
+           result+=term
+       return result
+   return P
+        
+
+n=6
+a=0
+x=np.linspace(-2,2,200)
+y=func(x)
+P5=taylor_polynominal(func,a,n)
+taylor_y=[P5(i) for i in x]
+
+plt.scatter(x,taylor_y, label=f"order 5 Taylor-sereis")
+plt.plot(x,y, label={fix_functions(expr)},color="Red")
+plt.axhline(0,color="Black")
+plt.axvline(0,color="Black")
+plt.grid(True)
+plt.legend()
+plt.show()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
